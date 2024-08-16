1e19225f531cc730516a266cb384ab1dfb2deefd
Egor Khindikaynen <khek@ambertape.ru>
2024-08-16 20:11:44 +0300

    Remove sukiui

--- src/Waves.Framework.UI.Avalonia/Controls/WavesWindow.cs
+++ src/Waves.Framework.UI.Avalonia/Controls/WavesWindow.cs
@@ -1,10 +1,9 @@
 using Avalonia.Controls;
-using SukiUI.Controls;
 using Waves.Framework.Presentation.Controls.Interfaces;
 
 namespace Waves.Framework.UI.Avalonia.Controls;
 
-public class WavesWindow : SukiWindow, IWavesWindow<object>
+public class WavesWindow : Window, IWavesWindow<object>
 {
     public virtual void Dispose()
     {
